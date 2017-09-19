5f74ccbab66e94b58e4cdc855841bb8e2ff0d7dc
Antonio <antonio.junior@luizalabs.com>
2017-09-19 11:10:58 -0300

    Adicionando loading para download

--- bin/util/download.js
+++ bin/util/download.js
@@ -1,15 +1,19 @@
 const download = require('download')
 const path = require('path');
 const fs = require('fs');
+const loading = require('loading-indicator');
 
 module.exports = (options) => {
     return new Promise((resolve, reject) => {
+      const timer = loading.start('Baixando...');
         download(options.url)
             .then(data => {
+                loading.stop(timer);
                 fs.writeFileSync(`${options.directory}//${options.filename}`, data);
                 return resolve(path.join(options.directory, options.filename));
             }).catch(err => {
-                return reject(err);
+              loading.stop(timer);  
+              return reject(err);
             });
     })
 }
