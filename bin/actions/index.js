28fe091480a8698c4450a861db5475e5ed64dbee
Antonio <antonio.junior@luizalabs.com>
2019-09-03 14:42:49 -0300

    Adicionando comando teresinha

--- bin/actions/index.js
+++ bin/actions/index.js
@@ -2,10 +2,12 @@ const checkUpdate = require('./checkUpdate');
 const download = require('./downloadTeresa');
 const env = require('./env');
 const start = require('./start');
+const getAllEnvs = require('./getAllEnvs');
 
 module.exports = {
     checkUpdate,
     download,
     env,
-    start
+    start,
+    getAllEnvs
 }
--- bin/actions/start.js
+++ bin/actions/start.js
@@ -1,9 +1,10 @@
 const { allApps } = require('../util/teresa');
+const command = require('../util/command');
 
 module.exports = async () => {
 
     const promises = allApps().map(app => {
-        return setCommand(`teresa app start ${app}`);
+        return command(`teresa app start ${app}`);
     })
 
     return Promise.all(promises);
