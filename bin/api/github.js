eb57451301f0603dbcc7f49a3952dd2e21050cf9
Antonio <jr_acn@yahoo.com.br>
2019-10-04 13:32:55 -0300

    fix axios

--- bin/api/github.js
+++ bin/api/github.js
@@ -1,6 +1,11 @@
 const { get } = require('axios');
 const webconfig = require('../../webconfig');
 
-module.exports = () => {
-    return get(`https://api.github.com/repos/luizalabs/teresa/releases/latest?access_token=${Buffer.from(webconfig.token, 'base64')}`);
+module.exports = async () => {
+  try {
+    const data = await get(`https://api.github.com/repos/luizalabs/teresa/releases/latest?access_token=${Buffer.from(webconfig.token, 'base64')}`);
+    return data.data;
+  } catch (error) {
+    throw error;
+  }
 }
