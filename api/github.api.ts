605846740e1569bef13ca540baf7d51963e59100
Antonio <antonio.junior@luizalabs.com>
2021-06-09 18:56:58 -0300

    fix: version

--- api/github.api.ts
+++ api/github.api.ts
@@ -1,13 +1,8 @@
 import axios from 'axios';
-import webconfig from '../config';
 
 export default async () => {
   try {
-    const data = await axios.get(`https://api.github.com/repos/luizalabs/teresa/releases/latest?access_token=${Buffer.from(webconfig.token, 'base64')}`);
-    if (data.data.documentation_url) {
-      console.error('Token Inválido.');
-      throw 'Token Inválido GITHUB';
-    }
+    const data = await axios.get('https://api.github.com/repos/luizalabs/teresa/releases/latest');
     return data.data;
   } catch (error) {
     throw error;
